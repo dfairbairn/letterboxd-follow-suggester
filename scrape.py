21a49167e985d4e4f7a2ecd6ba73f7b9d6ccddc1
dfairbairn <david.t.fairbairn@gmail.com>
2023-11-16 00:11:06 -0500

    Fix user table retrieval bug
     * Unpack usernames correctly
     * Also adds logging and a benign CLI check

--- scrape.py
+++ scrape.py
@@ -3,8 +3,11 @@ import sqlite3
 from scraping import members, user
 from datetime import datetime, timedelta
 import hashlib
+import time
+import random
 
 
+BACKOFF_TIME_BASE = 10.0
 SCRAPE_DB = "scrape_store.db"
 
 
@@ -64,6 +67,7 @@ class UsersScraper(Scraper):
         :return:
         """
         self.results = []
+        print(f"  [Debug] Prettifying top users results")
         for user in self._results:
             self.results.append(ScrapedUserObject(user))
         return self.results
@@ -77,11 +81,12 @@ class RatingScraper(Scraper):
          Precondition 2: self.results is empty (we're about to overwrite it if not)
          Assumption: scraping_function outputs those results in the same order as it did originally
         """
+        print(f"  [Debug] Prettifying user film ratings results for {user=}")
         self.results = []
         for rating in self._results:
             # Example entry in _results: (film_title_unreliable, film_id, film_url_pattern, rating, user )
             rating_val = RatingScraper.translate_stars(rating[3])
-            print(f"{rating} --------> {rating_val}")
+            # print(f"{rating} --------> {rating_val}")
             ro = RatingObject(
                 film_title=rating[0], film_id=rating[1], film_url=rating[2], film_rating=rating_val, user=rating[4])
             self.results.append(ro)
@@ -89,7 +94,6 @@ class RatingScraper(Scraper):
 
     @staticmethod
     def translate_stars(rating):
-
         rating_val = 0
         if "½" in rating:
             rating_val += 0.5
@@ -137,17 +141,18 @@ class ParsingStorage:
     def get_stale_users(self):
         stale_timestamp = int((datetime.utcnow() - timedelta(7)).timestamp())
         print(f"{stale_timestamp=}")
-        self.cursor.execute(f"SELECT * FROM users where last_updated < {stale_timestamp}")
+        self.cursor.execute(f"SELECT user FROM users where last_updated < {stale_timestamp}")
         users = self.cursor.fetchall()
-        print(f" Found {len(users)} members in DB needing a film rating update")
-        return users
+        users_strs = [u[0] for u in users]
+        print(f"  [Info] Found {len(users_strs)} members in DB needing a film rating update")
+        return users_strs
 
     def close(self):
         self.connection.close()
 
 
 def cli_get_top_members(get_top_members):
-    print(f"Get Top {get_top_members} Members")
+    print(f"[Info] Get Top {get_top_members} Members")
 
     db = ParsingStorage()
 
@@ -161,7 +166,7 @@ def cli_get_top_members(get_top_members):
 
 
 def cli_user_film_ratings(user_film_ratings):
-    print(f"User Film Ratings {user_film_ratings}")
+    print(f"[Info] User Film Ratings {user_film_ratings}")
 
     db = ParsingStorage()
 
@@ -172,32 +177,37 @@ def cli_user_film_ratings(user_film_ratings):
 
     # Test insertion into the DB
     for r in structured_ratings:
-        print(f"  {str(r)}")
+        print(f"  [Debug] {str(r)}")
         db.insert_rating(r)
     db.connection.commit()
 
 
-def cli_update_film_ratings(max_users_to_update=10, report_stale_users_only=False):
-    print(f"Update film ratings")
+def cli_update_film_ratings(max_users_to_update=100, report_stale_users_only=False):
+    print(f"[Info] Check user updates")
 
     db = ParsingStorage()
     stale_user_list = db.get_stale_users()
     db.close()
     if not report_stale_users_only:
         for i, stale_user in enumerate(stale_user_list):
+            print(f"  [{i}] - [Debug] Fetching film ratings for user '{stale_user}'...")
             if i >= max_users_to_update:
                 break
             cli_user_film_ratings(stale_user)
+            sleep_time = BACKOFF_TIME_BASE * (1 + random.random())
+            time.sleep(sleep_time)
 
 
 def main():
     parser = argparse.ArgumentParser("A scraper")
-    parser.add_argument('--get-top-members', dest="get_top_members",
+    parser.add_argument('--get-top-members', '-top', dest="get_top_members",
                         help="Scrape < N > most popular Letterboxd members of the last year"
                         " and insert into DB.")
-    parser.add_argument('--user-film-ratings', dest="user_film_ratings",
+    parser.add_argument('--user-film-ratings', '-ufr', dest="user_film_ratings",
                         help="Scrape all film ratings for < user >, insert into DB ")
-    parser.add_argument('--update-film-ratings', dest="update_film_ratings", action="store_true",
+    parser.add_argument('--users-to-update', '-utu', dest="users_to_update", action="store_true",
+                        help="Flag denoting to list users whose film ratings have not been scraped in the last 7 days")
+    parser.add_argument('--update-film-ratings', '-upd', dest="update_film_ratings", action="store_true",
                         help="Flag denoting to scrape film ratings for all users in DB not scraped in the last 7 days")
     args = parser.parse_args()
 
@@ -207,6 +217,9 @@ def main():
 
     elif args.user_film_ratings:
         cli_user_film_ratings(args.user_film_ratings)
+
+    elif args.users_to_update:
+        cli_update_film_ratings(report_stale_users_only=True)
 
     elif args.update_film_ratings:
         cli_update_film_ratings()
